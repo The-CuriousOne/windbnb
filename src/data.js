785c014e79b70aefd4f9e79c3c9cbf82480b0a87
The-CuriousOne <103219542+The-CuriousOne@users.noreply.github.com>
2022-09-18 23:29:08 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,212 @@
+export default [
+    {
+        id: 1,
+        title: "Luxury Room in China Town",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "china-room.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 6
+        },
+        location: "China Town",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "NIght in a Raft under stars",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "raft.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Goa",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Room in th Mountains of Ladakh",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "ladakh.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 20
+        },
+        location: "Ladakh",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "One day in the see in a Yacht",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 156,
+        coverImg: "yacht.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 27
+        },
+        location: "Goa",
+        openSpots: 27,
+    },
+    {
+        id: 5,
+        title: "A night under the stars",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 45,
+        coverImg: "cherry-hills.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 89
+        },
+        location: "Cherry Hills",
+        openSpots: 27,
+    },
+    {
+        id: 6,
+        title: "A splendid room with the view of the Golden Temple",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 200,
+        coverImg: "golden-temple.jpg",
+        stats: {
+            rating: 4.7,
+            reviewCount: 15
+        },
+        location: "Amritsar",
+        openSpots: 27,
+    },
+    {
+        id: 7,
+        title: "A splendid room with the view Sydney Bridge",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 89,
+        coverImg: "sydney-bridge.jpg",
+        stats: {
+            rating: 4.2,
+            reviewCount: 10
+        },
+        location: "Sydney",
+        openSpots: 27,
+    },
+    {
+        id: 8,
+        title: "A luxurious room in Venice City",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 145,
+        coverImg: "venice.jpg",
+        stats: {
+            rating: 4.6,
+            reviewCount: 19
+        },
+        location: "Venice",
+        openSpots: 27,
+    },
+    {
+        id: 9,
+        title: "A Room in New York City",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 405,
+        coverImg: "new-york.jpg",
+        stats: {
+            rating: 4.2,
+            reviewCount: 180
+        },
+        location: "New York",
+        openSpots: 27,
+    },
+    {
+        id: 10,
+        title: "A Room in Sedan",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 35,
+        coverImg: "sedan.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 700
+        },
+        location: "Sedan",
+        openSpots: 27,
+    },
+    {
+        id: 11,
+        title: "A Room in Abu Dabi",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 46,
+        coverImg: "saudi.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 524
+        },
+        location: "Abu Dabi",
+        openSpots: 27,
+    },
+    {
+        id: 11,
+        title: "An Appartment in Moscow",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 325,
+        coverImg: "russia.jpg",
+        stats: {
+            rating: 4.1,
+            reviewCount: 70
+        },
+        location: "Moscow",
+        openSpots: 27,
+    },
+    {
+        id: 12,
+        title: "An Appartment with the view of the Moscow Palace",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 67,
+        coverImg: "moscow-palace.jpg",
+        stats: {
+            rating: 4.0,
+            reviewCount: 131
+        },
+        location: "Moscow",
+        openSpots: 27,
+    },
+    {
+        id: 13,
+        title: "An luxurious room in the Burj Khalifa",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 5,
+        coverImg: "burj-khalifa.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 139
+        },
+        location: "Dubai",
+        openSpots: 27,
+    },
+    {
+        id: 15,
+        title: "A Splendid Appartment with the view of the Roman Cathedril",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 75,
+        coverImg: "roman-cathedril.jpg",
+        stats: {
+            rating: 4.3,
+            reviewCount: 19
+        },
+        location: "Rome",
+        openSpots: 27,
+    },
+    {
+        id: 16,
+        title: "A Room by the Sea",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 500,
+        coverImg: "room-by-the-sea.jpg",
+        stats: {
+            rating: 4.5,
+            reviewCount: 7000
+        },
+        location: "Middle of Nowhere",
+        openSpots: 27,
+    }
+
+
+]
